7e5055bfc911231f1a775b2f2f3c6a72c5cdcf0d
Daniel Lord-Doyle <dlorddoyle@vhslab.com>
2023-05-17 13:50:55 +1000

    add type file

--- src/index.ts
+++ src/index.ts
@@ -5,28 +5,46 @@ import User from "./models/User";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { Router } from "express";
+import dotenv from "dotenv";
+import { TokenPayload } from "./types";
 
 // Load environment variables
-import dotenv from "dotenv";
 dotenv.config();
 
+// Check APP_SECRET_KEY
 const { APP_SECRET_KEY, NODE_ENV } = process.env;
 if (!APP_SECRET_KEY) {
   console.error("Missing APP_SECRET_KEY");
   process.exit(1);
 }
 
-// Middleware
+// Express app setup
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
+// Token authentication middleware
+const authenticateToken = (req: any, res: any, next: any) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader)
+    return res.status(401).json({ message: "Access denied. Token missing." });
+  const token = authHeader.split(" ")[1];
+  if (!token)
+    return res.status(401).json({ message: "Access denied. Token missing." });
+  try {
+    const decoded = jwt.verify(token, APP_SECRET_KEY) as TokenPayload;
+    req.userId = decoded.id;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token." });
+  }
+};
+
+// Routes setup
 const authRouter = Router();
 const userRouter = Router();
 
-// auth routes
-
+// Auth routes
 authRouter.post("/register", async (req, res, next) => {
   try {
     const { username, password } = req.body;
@@ -41,44 +59,26 @@ authRouter.post("/login", async (req, res, next) => {
   try {
     const { username, password } = req.body;
     const user = await User.findOne({ where: { username } });
-    console.log(`Login attempt: User '${username}' is attempting to login.`);
-
-    if (!username || !password) {
-      console.log(
-        `Login attempt: user did not provide a username or password.`
-      );
+    if (!username || !password)
       return res
         .status(404)
         .json({ message: "Must provide username and password" });
-    }
-
-    if (!user) {
-      console.log(`Login attempt: No user matches the username: ${username}`);
-      return res.status(404).json({ message: "No user matches that username" });
-    }
-
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-      console.log(`Login attempt: Invalid username or password.`);
+    if (!user || !(await bcrypt.compare(password, user.password)))
       return res.status(401).json({ message: "Invalid username or password" });
-    }
-
     const payload = { id: user.id };
     const token = jwt.sign(payload, APP_SECRET_KEY);
-    console.log(`User ${user.username} has successfully logged in.`);
     res.json({ message: "ok", token });
   } catch (error) {
     next(error);
   }
 });
 
-// user routes
-
-userRouter.get("/", async (_, res, next) => {
+// User routes
+userRouter.get("/", authenticateToken, async (_, res, next) => {
   try {
     const users = await User.findAll();
-    if (!users || users.length === 0) {
+    if (!users || users.length === 0)
       return res.status(404).json({ message: "No Users found" });
-    }
     res.json(users);
   } catch (error) {
     next(error);
@@ -88,10 +88,7 @@ userRouter.get("/", async (_, res, next) => {
 userRouter.delete("/:id", async (req, res, next) => {
   try {
     const user = await User.findByPk(req.params.id);
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
+    if (!user) return res.status(404).json({ message: "User not found" });
     await user.destroy();
     res.json({ message: "User deleted successfully" });
   } catch (error) {
@@ -99,6 +96,7 @@ userRouter.delete("/:id", async (req, res, next) => {
   }
 });
 
+// Using routes
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
 
@@ -110,16 +108,16 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     .json({ message: err.message || "Internal server error" });
 });
 
-// Database setup, forcing a sync if in dev
+// Database setup and server start
 sequelize
   .authenticate()
-  .then(() => console.log("Database connected."))
+  .then(() => {
+    console.log("Database connected.");
+    sequelize.sync({ force: NODE_ENV === "development" }).then(() => {
+      console.log("Database synchronized!");
+      app.listen(3000, () => console.log("Server is listening on port 3000"));
+    });
+  })
   .catch((err) => console.error("Unable to connect to the database:", err));
 
-sequelize
-  .sync({ force: NODE_ENV === "development" })
-  .then(() => {
-    console.log("Database synchronized!");
-    app.listen(3000, () => console.log("Server is listening on port 3000"));
-  })
-  .catch((err) => console.error(err));
+export default app; // Add this line to export your app
