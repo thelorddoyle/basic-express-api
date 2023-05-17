7bdcb96d70a9be636746f297b6b471b383619616
Daniel Lord-Doyle <dlorddoyle@vhslab.com>
2023-05-17 11:47:03 +1000

    change db name to rocketlabs and see if we can still access users table ok

---
+++ src/migrations/20230517014054-create-books.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("books", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+    });
+  },
+  down: async (queryInterface, _) => {
+    await queryInterface.dropTable("books");
+  },
+};
