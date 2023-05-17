2752e9a6d212ddad63e8f64cb6a327e6513c9793
Daniel Lord-Doyle <dlorddoyle@vhslab.com>
2023-05-17 10:14:45 +1000

    change env var access

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -3,12 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+const { DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_PORT } = process.env;
 
 const sequelize = new Sequelize(
   DB_NAME as string,
   DB_USER as string,
-  DB_PASSWORD as string,
+  DB_PASS as string,
   {
     host: DB_HOST,
     port: Number(DB_PORT),
